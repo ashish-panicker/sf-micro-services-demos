5f73bae78f5c63c8a4bea9b7b03a2e3daf03265a
ashish-panicekr <ashish.s.panicekr@proton.me>
2025-11-11 10:30:46 +0530

    DB properties updated

--- course-service/src/main/java/com/example/courseservice/repository/CourseRepository.java
+++ course-service/src/main/java/com/example/courseservice/repository/CourseRepository.java
@@ -2,7 +2,7 @@ package com.example.courseservice.repository;
 
 import com.example.courseservice.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import java.util.List;
 
@@ -10,9 +10,17 @@ import java.util.List;
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     /**
-     *
      * @param instructor
      * @return
      */
     List<Course> findByInstructorContainingIgnoreCase(String instructor);
+
+    /**
+     * When the application starts
+     * 1. Spring will scan for all classes extending Repository
+     * 2. For each class it will create a proxy
+     * 3. The proxy will use SimpleJpaRepository internally to handle CRUD operations
+     * 4. SimpleJpaRepository uses jakarata.persistence.EntityManager for DB operations
+     */
+
 }
